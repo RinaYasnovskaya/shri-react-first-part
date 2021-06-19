7873f6a5c7d48dc10e16866bac632f0c36701d86
Rina Yasnovskaya <rina.yasnovskaya@gmail.com>
2021-06-20 02:29:42 +0300

    feat: add classe for elems footer & write some style

--- shri-react/src/components/footer/Footer.jsx
+++ shri-react/src/components/footer/Footer.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './footer.scss';
 
 export const Footer = () => {
   return (
     <div className="footer">
       <div className="footer__left-elem">
-        <Link to="/support">Support</Link>
-        <Link to="/learning">Learning</Link>
-        <button>Русская версия</button>
+        <Link className="footer__support" to="/support">Support</Link>
+        <Link className="footer__learning" to="/learning">Learning</Link>
+        <button className="footer__lang">Русская версия</button>
 
       </div>
       <div className="footer__right-elem">
