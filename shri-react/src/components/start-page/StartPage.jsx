b9ea9dc1cf4e4ae5fb6a1a9a43a2909eb21bfe3f
Rina Yasnovskaya <rina.yasnovskaya@gmail.com>
2021-06-20 12:30:57 +0300

    style: rewrite name class

--- shri-react/src/components/start-page/StartPage.jsx
+++ shri-react/src/components/start-page/StartPage.jsx
@@ -8,7 +8,7 @@ export const StartPage = () => {
       <div className="start-page__inner">
         <img src="../../assets/img/logo 3.svg" alt="" />
         <span>Configure repository connection and synchronization settings</span>
-        <Link to="/settings" className="button button_start button_bright">Open Settings</Link>
+        <Link to="/settings" className="button button__start button_bright">Open Settings</Link>
       </div>
     </div>
   );
