689b8f9ea09955c682ce7d444ab412c665618288
Rina Yasnovskaya <rina.yasnovskaya@gmail.com>
2021-06-20 14:35:10 +0300

    style: add styles for settings page

--- shri-react/src/components/settings/Settings.jsx
+++ shri-react/src/components/settings/Settings.jsx
@@ -1,33 +1,32 @@
 import React from 'react';
+import './settings.scss';
 
 export const Settings = () => {
   return (
-    <form action="">
-      <p className="form__title">
-        <span className="form__title-main" id="title_settings">Settings</span>
-        <span className="form__title-description" id="description_settings">
+    <form action="" className="form">
+      <div className="form__title">
+        <p className="form__title-main" id="title_settings">Settings</p>
+        <p className="form__title-description" id="description_settings">
           Configure repository connection and synchronization settings.
-        </span>
-      </p>
-      <div>
-        <label id="repo_name">
-          GitHub repository <span className="red-start">*</span>
-          <input required type="text" value="" onChange="" />
-        </label>
-        <label id="build_command">
-          Build Command <span className="red-start">*</span>
-          <input required type="text" value="" onChange="" />
-        </label>
-        <label id="build_command">
-          Main Branch
-          <input type="text" value="" onChange="" />
-        </label>
-        <label id="build_command">
-          Synchronize every <input type="number" value="" onChange="" /> minutes
-        </label>
-        <input type="submit" value="Submit" />
-        <input type="button" value="Cancel" />
+        </p>
       </div>
+      <div className="form__inner">
+        <label htmlFor="repo-name">
+          GitHub repository <span className="red-star">*</span>
+        </label>
+        <input required id="repo-name" type="text" placeholder="user-name/repo-name" />
+        <label id="build_command">
+          Build Command <span className="red-star">*</span>
+        </label>
+        <input required htmlFor="build-command" type="text" placeholder="build command: npm start" />
+        <label htmlFor="main-branch">Main Branch</label>
+        <input type="text" id="main-branch" placeholder="main branch: main" />
+        <label htmlFor="time">
+          Synchronize every <input id="time" type="number" /> minutes
+        </label>
+      </div>
+        <input type="submit" value="Save" className="button button_bright" />
+        <input type="button" value="Cancel" className="button button_light" />
     </form>
   );
 };
